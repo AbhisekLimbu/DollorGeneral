e218e56c60d596eeb4d4d1b2ab9037caf51411b4
Abhisek Phago limbu <112129568+AbhisekLimbu@users.noreply.github.com>
2024-02-01 19:48:47 -0600

    Update DollorGeneral.java
    Algorithm we will be using.

--- DollorGeneral.java
+++ DollorGeneral.java
@@ -1,6 +1,22 @@
+import java.util.ArrayList;
+
 public class DollarGeneral {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int numbers[] = {1, 2, 5, 10, 20, 50, 100};
+        ArrayList<Integer> array = new ArrayList<>();
+        int targetValue = 125;
+
+        for (int i = numbers.length - 1; i >= 0; i--) {
+            int denomination = numbers[i];
+            if (targetValue >= denomination) {
+                int count = targetValue / denomination;
+                for (int j = 0; j < count; j++) {
+                    array.add(denomination);
+                }
+                targetValue %= denomination;
+            }
+        }
+
+        System.out.println("Combination: " + array);
     }
 }
-o
